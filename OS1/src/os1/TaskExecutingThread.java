a6763f827c6dd7f69679275359b7b16b2cab376b
Avinoam <avinoam.zis@gmail.com>
2016-05-16 14:57:45 +0300

    Add Task Execute Thread

---
+++ OS1/src/os1/TaskExecutingThread.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os1;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author Avinoam
+ */
+public class TaskExecutingThread extends Thread {
+
+    private final ReentrantLock lock = new ReentrantLock(true);
+    private BlockingQueue taskQueue = null;
+    private boolean keepRunning = true;
+
+    public TaskExecutingThread(BlockingQueue queue) {
+        taskQueue = queue;
+    }
+
+    //
+    // starvation?
+    @Override
+    public void run() {
+        while (keepRunning()) {
+            try {
+                Runnable runnable = (Runnable) taskQueue.dequeue();
+                runnable.run();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void stopRunning() {
+        lock.lock();
+        try {
+            keepRunning = false;
+            this.interrupt();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean keepRunning() {
+        lock.lock();
+        try {
+            return keepRunning;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
